d7f761e41ecff63675ea1f88fa4f2b679e4afa60
HannaNurnazarova <nurnazarovaanna@gmail.com>
2024-07-28 23:34:19 +0300

    first commit

--- obj/Debug/net8.0/Seminar_01.AssemblyInfo.cs
+++ obj/Debug/net8.0/Seminar_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713d7b31f4bf439e3c1350b182679993b2b8dba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
